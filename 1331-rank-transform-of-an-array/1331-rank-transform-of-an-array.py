ebbc163890a8c8747895c74c3602a8dfac1c8d2f
THeMastermind <122424385+aryann911@users.noreply.github.com>
2026-07-12 21:14:18 +0530

    Time: 82 ms (10.7%), Space: 28.6 MB (39.09%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def arrayRankTransform(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        ranks = list(sorted(set(arr)))
+        for i in range(len(arr)):
+            arr[i] = bisect_left(ranks, arr[i]) + 1
+        return arr
