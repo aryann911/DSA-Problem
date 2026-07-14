6f7f7cead439cba291d8f4f4f9352df48fe08c0b
THeMastermind <122424385+aryann911@users.noreply.github.com>
2026-07-14 19:21:57 +0530

    Refactor maxArea method by removing docstring
    Removed type hints and docstring from maxArea method.

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
         left = 0
         right = len(height) - 1
         max_water = 0
