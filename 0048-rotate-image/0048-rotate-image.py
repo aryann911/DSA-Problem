4e3959a1bc83ebfbcc9567b522e2af4d31f65f16
THeMastermind <122424385+aryann911@users.noreply.github.com>
2026-07-21 09:31:04 +0530

    Time: 0 ms (100%), Space: 12.3 MB (59.83%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def rotate(self, matrix):
+        n=len(matrix)
+        for i in range(0,n-1):
+            for j in range(i+1,n):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+        for k in matrix:
+            k.reverse()
+
